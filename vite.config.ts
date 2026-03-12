02bc8d6cf7b28c8cf8ccec427379fe8ff1ffe87f
nazark-ship-it <nazar.k@phenomenon-studio.com>
2026-03-12 19:27:43 +0200

    fix(vite-configs): added host

--- vite.config.ts
+++ vite.config.ts
@@ -67,6 +67,7 @@ export default defineConfig(({ mode }) => {
         // Extra free PORTS if you need them (9199, 9889, 9521, 9836, 9713, 9407, 9491)
         server: {
             port: 9777,
+            allowedHosts: ['guardianship.lasco.org'],
         },
         preview: {
             port: 9111,
