33384622555ba5ba584fbb9be63852517856fac6
nazark-ship-it <nazar.k@phenomenon-studio.com>
2026-04-01 09:38:21 +0300

    fix(ui): turn back dev tools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,36 @@
+import React from 'react';
+import { TanStackDevtoolsReactInit } from '@tanstack/react-devtools';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { ToastNotificationContext } from '@/context/ToastNotificationContext';
 
+let TanStackDevtools: React.ComponentType<TanStackDevtoolsReactInit> = () => {
+    return null;
+};
+
+let TanStackRouterDevtoolsPanel: React.ComponentType<Record<string, never>> = () => {
+    return null;
+};
+
+let ReactQueryDevtoolsPanel: React.ComponentType<Record<string, never>> = () => {
+    return null;
+};
+
+if (import.meta.env.DEV) {
+    TanStackDevtools = React.lazy(async () => {
+        const res = await import('@tanstack/react-devtools');
+        return { default: res.TanStackDevtools };
+    });
+
+    TanStackRouterDevtoolsPanel = React.lazy(async () => {
+        const res = await import('@tanstack/router-devtools');
+        return { default: res.TanStackRouterDevtoolsPanel };
+    });
+
+    ReactQueryDevtoolsPanel = React.lazy(async () => {
+        const res = await import('@tanstack/react-query-devtools');
+        return { default: res.ReactQueryDevtoolsPanel };
+    });
+}
 export const Route = createRootRoute({
     head() {
         return {
@@ -20,6 +50,28 @@ export const Route = createRootRoute({
         return (
             <ToastNotificationContext>
                 <Outlet />
+
+                {import.meta.env.DEV && (
+                    <React.Suspense>
+                        <TanStackDevtools
+                            config={{
+                                position: 'bottom-right',
+                            }}
+                            plugins={[
+                                {
+                                    name: 'TanStack Query',
+                                    render: <ReactQueryDevtoolsPanel />,
+                                    defaultOpen: true,
+                                },
+                                {
+                                    name: 'TanStack Router',
+                                    render: <TanStackRouterDevtoolsPanel />,
+                                    defaultOpen: false,
+                                },
+                            ]}
+                        />
+                    </React.Suspense>
+                )}
             </ToastNotificationContext>
         );
     },
